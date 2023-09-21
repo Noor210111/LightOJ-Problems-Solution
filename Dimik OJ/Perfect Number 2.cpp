dbf4cefe795165d4a3965c306225912e11690ffe
Abu Mohammad Noor <noor.cse.pust@gmail.com>
2023-09-21 10:32:32 +0600

    Add files via upload

---
+++ Dimik OJ/Perfect Number 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        long long int n;
+        cin >> n;
+
+            if(n >= 6)
+                cout << 6 << endl;
+
+            if(n >= 28)
+                cout << 28 << endl;
+
+            if(n >= 496)
+                cout << 496 << endl;
+
+            if(n >= 8128)
+                cout << 8128 << endl;
+
+            if(n >= 33550336)
+                cout << 33550336 << endl;
+
+
+        if(t != 0);
+            cout << endl;
+    }
+    return  0;
+}
